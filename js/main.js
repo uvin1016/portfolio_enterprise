ac32a2c98d9649b15ab16fc9ae8f3fa746f5193a
uvin1016 <uvin1016@naver.com>
2021-12-18 21:45:38 +0900

    헤더에 마우스를 올렸을때 스크립트 추가

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+const body = document.querySelector("body");
 const header = document.querySelector("header");
 const headerHight = header.offsetHeight;
 const btnCall = document.querySelector(".btnCall");
@@ -9,26 +10,21 @@ window.onscroll = ()=>{
     if(windowTop >= headerHight){
         header.classList.add("on");
         btnCall.classList.add("on");
+
+        header.addEventListener("mouseenter",()=>{
+            header.classList.add("on");
+            btnCall.classList.add("on");
+        });
     }else{
         header.classList.remove("on");
         btnCall.classList.remove("on");
     }
 };
 
-header.addEventListener("mouseenter",()=>{
-    header.classList.add("on");
-})
-
-header.addEventListener("mouseleave",()=>{
-    header.classList.remove("on");
-    btnCall.classList.remove("on");
-})
-
 btnCall.onclick = function(e){
     e.preventDefault();
+
     btnCall.classList.toggle("on");
     menuMb.classList.toggle("on");
-
-    const menuHasOn = menuMb.classList.contains("on");
-    const logo = header.querySelector(".logo a");
+    body.classList.toggle("hidden");
 }
