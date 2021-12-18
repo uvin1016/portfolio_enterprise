b43e11fd74ee2e77824b749cb770e6f45894d88c
uvin1016 <uvin1016@naver.com>
2021-12-18 20:42:28 +0900

    모바일 메뉴, 스크롤 메뉴 동작

--- js/main.js
+++ js/main.js
@@ -1,12 +1,34 @@
 const header = document.querySelector("header");
 const headerHight = header.offsetHeight;
+const btnCall = document.querySelector(".btnCall");
+const menuMb = document.querySelector(".menuMb");
 
 window.onscroll = ()=>{
     let windowTop = window.scrollY;
 
     if(windowTop >= headerHight){
-        header.classList.add("scrollOn");
+        header.classList.add("on");
+        btnCall.classList.add("on");
     }else{
-        header.classList.remove("scrollOn");
+        header.classList.remove("on");
+        btnCall.classList.remove("on");
     }
 };
+
+header.addEventListener("mouseenter",()=>{
+    header.classList.add("on");
+})
+
+header.addEventListener("mouseleave",()=>{
+    header.classList.remove("on");
+    btnCall.classList.remove("on");
+})
+
+btnCall.onclick = function(e){
+    e.preventDefault();
+    btnCall.classList.toggle("on");
+    menuMb.classList.toggle("on");
+
+    const menuHasOn = menuMb.classList.contains("on");
+    const logo = header.querySelector(".logo a");
+}
