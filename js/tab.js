17c6c9569e91c0964b8cfb55f0d31218ccb41520
uvin1016 <uvin1016@naver.com>
2021-12-20 14:25:20 +0900

    community.html 수정

--- js/tab.js
+++ js/tab.js
@@ -1,5 +1,7 @@
 const btns = document.querySelectorAll(".tabBtns > li");
 const boxs = document.querySelectorAll(".tabBoxs > div");
+const faqTitle = document.querySelectorAll(".faq dl dt");
+const faqSub = document.querySelectorAll(".faq dl dd");
 
 btns.forEach((el,index)=>{
     el.addEventListener("click",e=>{
@@ -12,6 +14,18 @@ btns.forEach((el,index)=>{
         activation(boxs, index);
     });
 });
+
+faqTitle.forEach((el,index)=>{
+    el.addEventListener("click",e=>{
+        e.preventDefault();
+
+        let isOn = e.currentTarget.classList.contains("on");
+        if(isOn) return;
+
+        activation(faqTitle, index);
+        activation(faqSub, index);
+    })
+})
 
 function activation(arr,index){
     for(let item of arr){
