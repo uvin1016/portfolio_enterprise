79909d46f3c9e747f5f0081b4feb7ded3c5a4191
uvin1016 <uvin1016@naver.com>
2022-01-24 18:35:20 +0900

    about 섹션 스크롤시 숫자 카운트 증가 스크립트 추가

--- js/main.js
+++ js/main.js
@@ -3,16 +3,23 @@ const header = document.querySelector("header");
 const headerHight = header.offsetHeight;
 const btnCall = document.querySelector(".btnCall");
 const menuMb = document.querySelector(".menuMb");
+const secs = document.querySelectorAll("section");
 const newsList = document.querySelectorAll("#news .inner ul li");
+const counter = document.querySelectorAll(".counter");
+let active = true;
+let posArr = [];
+
+setPos();
 
 header.addEventListener("mouseenter",()=>{
     header.classList.add("on");
     btnCall.classList.add("on");
 });
 
+
 window.onscroll = ()=>{
-    let windowTop = window.scrollY;
-    
+    let windowTop = window.scrollY || window.pageYOffset;
+
     if(windowTop >= headerHight){
         header.classList.add("on");
         btnCall.classList.add("on");
@@ -21,7 +28,15 @@ window.onscroll = ()=>{
         header.classList.remove("on");
         btnCall.classList.remove("on");
     }
+
+    if(windowTop >= posArr[0] - 300 ){
+        if( active == true) {
+            for(let i=0; i<counter.length; i++) countUp(i);
+        }
+        active = false;
+    }
 };
+
 
 btnCall.onclick = function(e){
     e.preventDefault();
@@ -42,9 +57,26 @@ newsList.forEach((el,index)=>{
     })
 })
 
+
 function activation(arr,index){
     for(let item of arr){
         item.classList.remove("on");
     }
     arr[index].classList.add("on");
+}
+
+function setPos(){
+    posArr = [];
+    for(let el of secs) posArr.push(el.offsetTop);
+}
+
+function countUp(index){
+    let num = 0;
+    let targetNum = counter[index].getAttribute('data-rate');
+
+    let timer = setInterval(()=>{
+        ++num;
+        counter[index].innerText = num;
+        if(num == targetNum) clearInterval(timer);
+    },50);
 }
